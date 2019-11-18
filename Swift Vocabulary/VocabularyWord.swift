5f709719713bc18d1c2129c712997b2e765b9776
Gerardo Hernandez <gahernandez02@gmail.com>
2019-11-17 18:40:15 -0800

    Programming portion completed for pull

--- Swift Vocabulary/VocabularyWord.swift
+++ Swift Vocabulary/VocabularyWord.swift
@@ -20,10 +20,14 @@ struct VocabularyWord {
     
 }
 
-let variable = VocabularyWord(word: "Varible", definition: "A name value used to store information. Variables can be changed after being created.")
+
+let function = VocabularyWord(word: "Function", definition: "Functions are self-contained chunks of code that perform a specific task. You give a function a name that identifies what it does, and this name is used to “call” the function to perform its task when needed.")
+
+
 
 let constant = VocabularyWord(word: "Constant", definition: "Constants refer to fixed values that a program may not alter during its execution. Constants can be of any of the basic data types like an integer constant, a floating constant, a character constant, or a string literal. There are enumeration constants as well.")
 
-let function = VocabularyWord(word: "Function", definition: "Functions are self-contained chunks of code that perform a specific task. You give a function a name that identifies what it does, and this name is used to “call” the function to perform its task when needed.")
+let variable = VocabularyWord(word: "Varible", definition: "A name value used to store information. Variables can be changed after being created.")
+
 
 //let swiftDefinitions: [VocabularyWord] = [variable, constant, function]
